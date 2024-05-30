369d43c312c1faf54025a418920f4fa50179d395
Dennis "D.C." Dietrich <44212152+dennisdietrich@users.noreply.github.com>
2024-05-30 15:30:24 +0200

    Initial check in of fast memory clear

---
+++ ClearMemory/src/clearmem.asm
@@ -0,0 +1,40 @@
+*=$c000                ; Start of upper RAM area
+
+!addr start = $fb      ; Inclusive start address of memory to clear, also current page
+!addr end   = $fd      ; Inclusive end address of memory to clear
+
+    lda #$00
+    ldy start          ; Store start index of first page to clear in Y
+    sta start          ; Clear low byte of start address
+
+clearpage
+
+    ldx start + 1      ; If current page is end page go to clearfinalpage
+    cpx end + 1
+    beq clearfinalpage
+
+clearbytes
+
+    sta (start),Y      ; Clear current byte
+    cpy #$ff           ; If last byte of page was cleared go to nextpage
+    beq nextpage
+    iny                ; Prepare next loop iteration
+    jmp clearbytes
+
+nextpage
+
+    ldy #$00           ; Increment page and reset page offset
+    inc start + 1
+    jmp clearpage
+
+clearfinalpage
+
+    sta (start),Y      ; Clear current byte
+    cpy end            ; if last byte of last page was cleared return
+    beq return
+    iny                ; Prepare next loop interation
+    jmp clearfinalpage
+
+return
+
+    rts
